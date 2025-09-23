6035c7364f6c4f6a9ae4ecb4cf95c21a1d1d13ab
LeviTorresx <levist42@gmail.com>
2025-09-23 16:09:41 -0500

    ajustes a solucion wgrep

--- enunciado/wget/wgrep.c
+++ enunciado/wget/wgrep.c
@@ -3,49 +3,56 @@
 #include <string.h>
 #include <sys/types.h>
 
+char *line = NULL;
+size_t len = 0;
+ssize_t read;
+
+void cleanup() {
+    free(line);
+}
+
+void print_error_and_exit(const char *msg) {
+    printf("%s\n", msg);
+    exit(1);
+}
+
+void search_in_stream(FILE *fp, const char *searchterm) {
+    while ((read = getline(&line, &len, fp)) != -1) {
+        if (strstr(line, searchterm) != NULL) {
+            printf("%s", line);
+        }
+    }
+}
+
+void process_files(int argc, char *argv[], const char *searchterm) {
+    if (argc == 2) {
+        
+        // rd stdin
+        search_in_stream(stdin, searchterm);
+        return;
+    }
+
+    // rd of files
+    for (int i = 2; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            print_error_and_exit("wgrep: cannot open file");
+        }
+        search_in_stream(fp, searchterm);
+        fclose(fp);
+    }
+}
 
 int main(int argc, char *argv[]) {
+    atexit(cleanup);
+
     if (argc < 2) {
         printf("wgrep: searchterm [file ...]\n");
         return 1;
     }
-    
+
     char *searchterm = argv[1];
-
-    if (argc == 2) {
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t read;
-
-        while ((read = getline(&line, &len, stdin)) != -1) {
-            if (strstr(line, searchterm) != NULL) {
-                printf("%s", line);
-            }
-        }
-        free(line);
-        return 0;
-    }
-
-    for (int i = 2; i < argc; i++) {
-        FILE *fp = fopen(argv[i], "r");
-        if (fp == NULL) {
-            printf("wgrep: cannot open file\n");
-            return 1;
-        }
-
-        char *line = NULL;
-        size_t len = 0;
-        ssize_t read;
-
-        while ((read = getline(&line, &len, fp)) != -1) {
-            if (strstr(line, searchterm) != NULL) {
-                printf("%s", line);
-            }
-        }
-
-        free(line);
-        fclose(fp);
-    }
+    process_files(argc, argv, searchterm);
 
     return 0;
 }

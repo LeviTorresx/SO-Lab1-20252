78b7308cbb23df8ae8ce595ff8464e09a99cbdcc
LeviTorresx <levist42@gmail.com>
2025-09-23 16:57:53 -0500

    ajuste a solucion wcat

--- enunciado/wcat/wcat.c
+++ enunciado/wcat/wcat.c
@@ -1,28 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-    if (argc == 1)
-    {
+// impresion
+int print_file(const char *filename) {
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("wcat: cannot open file\n");
+        return 1; // error
+    }
+
+    char buffer[4096]; // buffer 
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        printf("%s", buffer);
+    }
+
+    fclose(fp);
+    return 0; // success
+}
+
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
         return 0; // nothing to do
     }
 
-    for (int i = 1; i < argc; i++)
-    {
-        FILE *fp = fopen(argv[i], "r");
-        if (fp == NULL)
-        {
-            printf("wcat: cannot open file\n");
+    for (int i = 1; i < argc; i++) {
+        if (print_file(argv[i]) != 0) {
             return 1; // error
         }
-        char buffer[4096]; // buffer
-        while (fgets(buffer, sizeof(buffer), fp) != NULL)
-        {
-            printf("%s", buffer);
-        }
-        fclose(fp);
     }
 
-    return 0; // melo
+    return 0; // success
 }
